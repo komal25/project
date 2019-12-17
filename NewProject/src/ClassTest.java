1f5608fbb665879d0fea13d39aa00348f39d95f2
komal bhure <bhurekomal@gmail.com>
2019-12-17 18:42:23 +0530

    branch created

--- NewProject/src/ClassTest.java
+++ NewProject/src/ClassTest.java
@@ -3,5 +3,8 @@ public class ClassTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello git");
+		System.out.println("WElcome to git");
+		System.out.println("new branch created");
+
 	}
 }
